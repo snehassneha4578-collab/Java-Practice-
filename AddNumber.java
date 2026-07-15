3e798e870b09a28c208146be8cc7f7e894e74921
Sneha S <snehassneha4578@gmail.com>
2026-07-15 11:58:34 +0530

    Add files via upload
    AddNumber.java

---
+++ AddNumber.java
@@ -0,0 +1,72 @@
+class AddNumbers
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 10;
+
+        int sum = a + b;
+
+        System.out.println("First Number  : " + a);
+        System.out.println("Second Number : " + b);
+        System.out.println("Sum = " + sum);
+    }
+}
+class SubtractNumbers
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 10;
+
+        int difference = a - b;
+
+        System.out.println("First Number  : " + a);
+        System.out.println("Second Number : " + b);
+        System.out.println("Difference = " + difference);
+    }
+}
+class MultiplyNumbers
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 10;
+
+        int product = a * b;
+
+        System.out.println("First Number  : " + a);
+        System.out.println("Second Number : " + b);
+        System.out.println("Product = " + product);
+    }
+}
+class DivideNumbers
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 10;
+
+        int quotient = a / b;
+
+        System.out.println("First Number  : " + a);
+        System.out.println("Second Number : " + b);
+        System.out.println("Quotient = " + quotient);
+    }
+}
+
+class RemainderNumbers
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 6;
+
+        int remainder = a % b;
+
+        System.out.println("First Number  : " + a);
+        System.out.println("Second Number : " + b);
+        System.out.println("Remainder = " + remainder);
+    }
+}
+
