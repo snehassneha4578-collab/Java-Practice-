b499159970251f454b5c3de27ffe4239c1d17e5c
Sneha S <snehassneha4578@gmail.com>
2026-07-16 08:41:12 +0530

    Add files via upload
    Increment Operator

---
+++ IncrementDemo.java
@@ -0,0 +1,11 @@
+class IncrementDemo
+{
+    public static void main(String[] args)
+    {
+        int number = 10;
+
+        number++;
+
+        System.out.println("After Increment: " + number);
+    }
+}
