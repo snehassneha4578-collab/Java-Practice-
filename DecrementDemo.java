28a68e2af92f5cfe9f1ebdf162872b71dedf3365
Sneha S <snehassneha4578@gmail.com>
2026-07-16 08:40:34 +0530

    Add files via upload
    Decrement Operator

---
+++ DecrementDemo.java
@@ -0,0 +1,11 @@
+class DecrementDemo
+{
+    public static void main(String[] args)
+    {
+        int number = 10;
+
+        number--;
+
+        System.out.println("After Decrement: " + number);
+    }
+}
