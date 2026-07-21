fb511983d098043c7abac62064b42c873f169d84
Sneha S <snehassneha4578@gmail.com>
2026-07-21 07:09:10 +0530

    Add files via upload

---
+++ PositiveNumber.java
@@ -0,0 +1,12 @@
+class PositiveNumber
+{
+    public static void main(String[] args)
+    {
+        int number = 15;
+
+        if(number > 0)
+        {
+            System.out.println("Positive Number");
+        }
+    }
+}
