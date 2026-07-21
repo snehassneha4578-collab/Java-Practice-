3f51efa7673c4240c569ba563d269e360cb2568c
Sneha S <snehassneha4578@gmail.com>
2026-07-21 07:08:01 +0530

    Add files via upload

---
+++ DivisibleByFive.java
@@ -0,0 +1,12 @@
+class DivisibleByFive
+{
+    public static void main(String[] args)
+    {
+        int number = 25;
+
+        if(number % 5 == 0)
+        {
+            System.out.println("Number is divisible by 5");
+        }
+    }
+}
