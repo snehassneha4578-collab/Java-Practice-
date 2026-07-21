e6dd1eb0da0798961fd566bdbadace533e7cb158
Sneha S <snehassneha4578@gmail.com>
2026-07-21 07:08:25 +0530

    Add files via upload

---
+++ GreaterThanHundred.java
@@ -0,0 +1,12 @@
+class GreaterThanHundred
+{
+    public static void main(String[] args)
+    {
+        int number = 150;
+
+        if(number > 100)
+        {
+            System.out.println("Number is greater than 100");
+        }
+    }
+}
