399cddcf09371b2b0aad8ac23686d3a4f7042391
Sneha S <snehassneha4578@gmail.com>
2026-07-21 07:08:48 +0530

    Add files via upload

---
+++ PassCheck.java
@@ -0,0 +1,12 @@
+class PassCheck
+{
+    public static void main(String[] args)
+    {
+        int marks = 72;
+
+        if(marks >= 35)
+        {
+            System.out.println("Pass");
+        }
+    }
+}
