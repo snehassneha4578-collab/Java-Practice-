7eb7719a25d77ad52c8f4892842e422ccdeea7c3
Sneha S <snehassneha4578@gmail.com>
2026-07-26 06:02:25 +0530

    Add files via upload

---
+++ TableOfSeven.java
@@ -0,0 +1,10 @@
+class TableOfSeven
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println("7 x " + i + " = " + (7 * i));
+        }
+    }
+}
