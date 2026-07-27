88c08e47923f1d3e8791f332949319c1f57d63d4
Sneha S <snehassneha4578@gmail.com>
2026-07-27 05:50:16 +0530

    Add files via upload

---
+++ BreakProgram.java
@@ -0,0 +1,15 @@
+class BreakProgram
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            if(i == 7)
+            {
+                break;
+            }
+
+            System.out.println(i);
+        }
+    }
+}
